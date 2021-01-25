7187b23c585a7dbb356e93a4f4298c1bdb806513
Alex Radzevich <Alex.Radzevich@deltatre.com>
2021-01-25 20:47:17 +0300

    initial commit

---
+++ TMS.Net07.Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TMS.Net07.Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
